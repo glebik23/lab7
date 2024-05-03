1a0e34d58e541d22fdad4e97d9ffcf8b86f2106f
glebik23 <skuryat.png@gmail.com>
2024-05-03 23:27:25 +0300

    Создание панели управления

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -4,17 +4,10 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.rememberCoroutineScope
-import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
-import com.topic2.android.notes.ui.components.AppDrawer
-import com.topic2.android.notes.ui.components.Note
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
-import kotlinx.coroutines.launch
+import com.topic2.android.notes.ui.components.screen.NoteScreen
 
 /**
  * Main activity приложения.
