3fa270ae174c6c12bb6dea054a16d2efff8c3083
glebik23 <skuryat.png@gmail.com>
2024-05-07 23:03:49 +0300

    Создание содержимого заметки сохранения

--- app/src/main/java/com/topic2/android/notes/ui/components/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/SaveNoteScreen.kt
@@ -3,7 +3,9 @@ package com.topic2.android.notes.ui.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
@@ -110,6 +112,42 @@ private fun SaveNoteTopAppBar(
             }
         }
     )
+}
+
+@Composable
+private fun SaveNoteContent(
+    note: NoteModel,
+    onNoteChange: (NoteModel) -> Unit
+) {
+    Column (modifier = Modifier.fillMaxSize()) {
+        ContentTextField(
+            label = "Title",
+            text = note.title,
+            onTextChange = { newTitle ->
+                onNoteChange.invoke(note.copy(title = newTitle))
+            }
+        )
+        ContentTextField(
+            modifier = Modifier
+                .heightIn(max = 240.dp)
+                .padding(top = 16.dp),
+            label = "Body",
+            text = note.content,
+            onTextChange = { newContent ->
+                onNoteChange.invoke(note.copy(content = newContent))
+            }
+        )
+        val canBeCheckedOff: Boolean = note.isCheckedOff != null
+
+        NoteCheckOption(
+            isChecked = canBeCheckedOff,
+            onCheckedChange = { canBeCheckedOffNewValue ->
+                val isCheckedOff: Boolean? = if (canBeCheckedOffNewValue) false else null
+                onNoteChange.invoke(note.copy(isCheckedOff = isCheckedOff))
+            }
+        )
+        PickedColor(color = note.color)
+    }
 }
 
 @Composable
@@ -257,6 +295,15 @@ fun SaveNoteTopAppBarPreview() {
         onSaveNoteClick = {},
         onOpenColorPickerClick = {},
         onDeleteNoteClick = {}
+    )
+}
+
+@Preview
+@Composable
+fun SaveNoteContentPreview() {
+    SaveNoteContent(
+        note = NoteModel(title = "Title", content = "content"),
+        onNoteChange = {}
     )
 }
 
