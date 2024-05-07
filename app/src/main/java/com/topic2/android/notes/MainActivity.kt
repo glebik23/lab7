ccb6f4daf4b05ecc4ede9ff26fbfab9027888a06
glebik23 <skuryat.png@gmail.com>
2024-05-07 21:08:18 +0300

    Подключение компонуемого к MainActivity

--- app/src/main/java/com/topic2/android/notes/MainActivity.kt
+++ app/src/main/java/com/topic2/android/notes/MainActivity.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import com.topic2.android.notes.routing.NotesRouter
+import com.topic2.android.notes.routing.Screen
 import com.topic2.android.notes.theme.NotesTheme
+import com.topic2.android.notes.ui.components.SaveNoteScreen
 import com.topic2.android.notes.viewmodel.MainViewModel
 import com.topic2.android.notes.viewmodel.MainViewModelFactory
 import com.topic2.android.notes.ui.components.screen.NoteScreen
+import com.topic2.android.notes.ui.screens.TrashScreen
 
 /**
  * Main activity приложения.
@@ -28,6 +35,18 @@ class MainActivity : AppCompatActivity() {
       NotesTheme {
         NoteScreen(viewModel = viewModel)
       }
+    }
+  }
+}
+
+@Composable
+@ExperimentalMaterialApi
+private fun MainActivityScreen(viewModel: MainViewModel) {
+  Surface {
+    when (NotesRouter.currentScreen) {
+      is Screen.Notes -> NoteScreen(viewModel)
+      is Screen.SaveNote -> SaveNoteScreen(viewModel)
+      is Screen.Trash -> TrashScreen(viewModel)
     }
   }
 }
