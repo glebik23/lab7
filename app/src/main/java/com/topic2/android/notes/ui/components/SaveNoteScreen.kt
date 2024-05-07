ffb2c8615db94b1ea5f084177794b3b99c23c346
glebik23 <skuryat.png@gmail.com>
2024-05-07 21:49:32 +0300

    Отображение выбранного цвета

--- app/src/main/java/com/topic2/android/notes/ui/components/SaveNoteScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/SaveNoteScreen.kt
@@ -110,6 +110,26 @@ private fun SaveNoteTopAppBar(
 }
 
 @Composable
+private fun PickedColor(color: ColorModel) {
+    Row (
+        Modifier
+            .padding(8.dp)
+            .padding(top = 16.dp)
+    ) {
+        Text(
+            text = "Picked color", modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        )
+        NoteColor(
+            color = Color.fromHex(color.hex), size = 40.dp,
+            border = 1.dp,
+            modifier = Modifier.padding(4.dp)
+        )
+    }
+}
+
+@Composable
 private fun ColorPicker(
     colors: List<ColorModel>,
     onColorSelect: (ColorModel) -> Unit
@@ -195,4 +215,10 @@ fun SaveNoteTopAppBarPreview() {
         onOpenColorPickerClick = {},
         onDeleteNoteClick = {}
     )
+}
+
+@Preview
+@Composable
+fun PickedColorPreview() {
+    PickedColor(ColorModel.DEFAULT)
 }
